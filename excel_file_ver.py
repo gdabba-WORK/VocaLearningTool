2a592bb90b779bb469ddcc00e16cf67882a7ac41
gdabba-WORK <kgdfather@gmail.com>
2021-05-17 22:54:44 +0900

    ==Excel 파일(.xlsx) 기반 영단어 학습기 작업 완료== -기능요약: 영단어와 뜻이 작성 된 입력파일(vocabulary.xlsx)을 프로그램으로 처리하면 출력파일(vocabulary_quiz.xlsx)에 영단어와 뜻이 뒤섞인 sheet(origin) 한 개 및 영단어 또는 뜻이 지워진 sheet(quiz)가 생성된다.
    -개선사항: 기존의 텍스트 또는 '.odt'확장자 기반의 프로그램은 영단어 및 뜻 정렬이 쉽지 않고, 인쇄물로 일정하게 출력하기도 어려웠다. 엑셀 기반으로 변경되며 이러한 문제점이 해결되었다.

---
+++ excel_file_ver.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import re
+from styleframe import StyleFrame
+import random
+
+
+def func01():
+    sheet_range = [21, 22]
+    df_origin = pd.DataFrame()
+
+    for num in sheet_range:
+        excel_sheet_name = re.sub('00', str(num), 'day00')
+        df_origin = df_origin.append(pd.read_excel("data/vocabulary.xlsx", sheet_name=excel_sheet_name, header=None),
+                       ignore_index=True)
+
+    shuffled_df = df_origin.iloc[np.random.permutation(df_origin.index)].reset_index(drop=True)
+    shuffled_df_len = len(shuffled_df.index)
+    df_origin = pd.DataFrame(index=[1, 2, 3, 4])
+    df_quiz = pd.DataFrame(index=[1, 2, 3, 4])
+    for num in range(0, shuffled_df_len, 2):
+        df_origin.loc[int(num/2), 1] = shuffled_df.loc[num, 0]
+        df_origin.loc[int(num/2), 2] = shuffled_df.loc[num, 1]
+        df_origin.loc[int(num/2), 3] = shuffled_df.loc[num+1, 0]
+        df_origin.loc[int(num/2), 4] = shuffled_df.loc[num+1, 1]
+
+        seed = random.choice(['VOCA', 'MEANING'])
+        if seed == 'VOCA':
+            df_quiz.loc[int(num/2), 1] = shuffled_df.loc[num, 0]
+            df_quiz.loc[int(num/2), 2] = ''
+        elif seed == 'MEANING':
+            df_quiz.loc[int(num/2), 1] = ''
+            df_quiz.loc[int(num/2), 2] = shuffled_df.loc[num, 1]
+        seed = random.choice(['VOCA', 'MEANING'])
+
+        if seed == 'VOCA':
+            df_quiz.loc[int(num/2), 3] = shuffled_df.loc[num+1, 0]
+            df_quiz.loc[int(num/2), 4] = ''
+        elif seed == 'MEANING':
+            df_quiz.loc[int(num/2), 3] = ''
+            df_quiz.loc[int(num/2), 4] = shuffled_df.loc[num+1, 1]
+
+    excel_writer = StyleFrame.ExcelWriter('data/vocabulary_quiz.xlsx')
+    sf_origin = StyleFrame(df_origin)
+    sf_quiz = StyleFrame(df_quiz)
+    # sf.set_row_height(rows=[n for n in range(1, math.ceil(shuffled_df_len/2)+1)], height=)
+    sf_origin.set_column_width(columns=['A', 'C'], width=16.0)
+    sf_origin.set_column_width(columns=['B', 'D'], width=40.0)
+    sf_quiz.set_column_width(columns=['A', 'C'], width=16.0)
+    sf_quiz.set_column_width(columns=['B', 'D'], width=40.0)
+
+    sf_origin.to_excel(excel_writer, header=False, index=False, sheet_name='origin')
+    sf_quiz.to_excel(excel_writer, header=False, index=False, sheet_name='quiz')
+    excel_writer.save()
---
+++ text_editor_ver.py
@@ -0,0 +1,18 @@
+def read_write():
+    file_read = open('raw.odt', 'r')
+    file_write = open('processed.odt', 'w')
+
+    data = file_read.readline()
+    data_dict = {}
+
+    while data:
+        data_list = data.split("\t", 1)
+        data_dict[data_list[0]] = data_list[1].strip('\t')
+        data = file_read.readline()
+
+    list_keys = list(data_dict.keys())
+    # list_values = data_list.values()
+    for i in range(0, len(list_keys)):
+        file_write.write(list_keys[i] + "\t\t" + data_dict[list_keys[i]] + "\n")
+    file_read.close()
+    file_write.close()
